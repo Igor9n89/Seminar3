e3dce374d9639c71196654b6a94eb26b279f3c54
Igor Ivanov <iii3-89@mail.ru>
2022-10-11 21:08:23 +0300

    Внес решение 23 задачи

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) выдает таблицу кубов
+// чисел от 1 до N
+// 3 и 5
+
+void Thr(int n)
+{
+   for (int i = 1; i <= n; i++)
+   {  
+       Console.Write($"{i*i*i}");
+       if (i != n)
+       {
+           Console.Write(", ");
+       }
+
+   }
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Thr(N);
